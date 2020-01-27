a8a355a6818030e774dc2acdd8541e66e15f100a
Shabie Iqbal <shabieiqbal@gmail.com>
2020-01-27 21:45:42 +0100

    Complete code for creating Turnstile producer simulating passengers

--- Project - Optimizing Public Transportation/producers/models/turnstile.py
+++ Project - Optimizing Public Transportation/producers/models/turnstile.py
@@ -15,34 +15,33 @@ class Turnstile(Producer):
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
 
     #
-    # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
+    # TODO: Define this value schema in `schemas/turnstile_value.json, then
+    #       uncomment the below
     #
-    #value_schema = avro.load(
-    #    f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
-    #)
+    value_schema = avro.load(
+       f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+    )
 
     def __init__(self, station):
         """Create the Turnstile"""
-        station_name = (
-            station.name.lower()
-            .replace("/", "_and_")
-            .replace(" ", "_")
-            .replace("-", "_")
-            .replace("'", "")
-        )
 
         #
+        # TODO: Complete the below by deciding on a topic name, number of
+        #       partitions, and number of replicas
         #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
+
+        # TODO: Include/fill the following in the call to super.__init__():
+        #       value_schema=Station.value_schema,
+        #       num_partitions=???,
+        #       num_replicas=???,
+
         super().__init__(
-            f"{station_name}", # TODO: Come up with a better topic name
+            # TODO: Come up with a better topic name
+            topic_name="org.chicago.cta.station.turnstile.v1",
             key_schema=Turnstile.key_schema,
-            # TODO: value_schema=Turnstile.value_schema, TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Turnstile.value_schema,
+            num_partitions=3,
+            num_replicas=1,
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
@@ -50,10 +49,27 @@ class Turnstile(Producer):
     def run(self, timestamp, time_step):
         """Simulates riders entering through the turnstile."""
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
-        logger.info("turnstile kafka integration incomplete - skipping")
         #
         #
-        # TODO: Complete this function by emitting a message to the turnstile topic for the number
-        # of entries that were calculated
+        # TODO: Complete this function by emitting a message to the turnstile
+        #       topic for the number of entries that were calculated
         #
-        #
+        msg = f"{num_entries} riders have entered station {self.station.name}" \
+              f" at {timestamp.isoformat()}"
+        logger.debug(msg)
+
+        for _ in range(num_entries):
+            try:
+                k = {"timestamp": self.time_millis()}
+                v = {
+                    "station_id": self.station.station_id,
+                    "station_name": self.station.name,
+                    "line": self.station.color.name,
+                }
+                self.producer.produce(topic=self.topic_name,
+                                      key=k,
+                                      value=v,
+                                      )
+            except Exception as e:
+                logger.fatal(e)
+                raise
