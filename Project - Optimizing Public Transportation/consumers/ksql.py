683679cfc784bd919bd8ed316626f3a7baf1ced3
Shabie Iqbal <shabieiqbal@gmail.com>
2020-01-28 23:18:47 +0100

    Complete ksql.py

--- Project - Optimizing Public Transportation/consumers/ksql.py
+++ Project - Optimizing Public Transportation/consumers/ksql.py
@@ -14,23 +14,31 @@ KSQL_URL = "http://localhost:8088"
 
 #
 # TODO: Complete the following KSQL statements.
-# TODO: For the first statement, create a `turnstile` table from your turnstile topic.
-#       Make sure to use 'avro' datatype!
-# TODO: For the second statment, create a `turnstile_summary` table by selecting from the
-#       `turnstile` table and grouping on station_id.
+
+# TODO: For the first statement, create a `turnstile` table from your turnstile
+#       topic. Make sure to use 'avro' datatype!
+
+# TODO: For the second statment, create a `turnstile_summary` table by selecting
+#       from the `turnstile` table and grouping on station_id.
 #       Make sure to cast the COUNT of station id to `count`
 #       Make sure to set the value format to JSON
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
-    ???
+    station_id INT,
+    station_name VARCHAR,
+    line VARCHAR
 ) WITH (
-    ???
+    kafka_topic = "org.chicago.cta.station.turnstile.v1",
+    value_format = "avro",
+    key = "station_id"
 );
 
 CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+WITH (value_format = "json") AS
+    SELECT station_id, COUNT(station_id) AS count
+    FROM turnstile
+    GROUP BY station_id;
 """
 
 
@@ -47,8 +55,10 @@ def execute_statement():
         data=json.dumps(
             {
                 "ksql": KSQL_STATEMENT,
-                "streamsProperties": {"ksql.streams.auto.offset.reset": "earliest"},
-            }
+                "streamsProperties": {
+                    "ksql.streams.auto.offset.reset": "earliest"
+                },
+            },
         ),
     )
 
