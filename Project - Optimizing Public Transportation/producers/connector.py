e75efeb6b0d2d39a0805bb9694aff4b1f0a819c1
Shabie Iqbal <shabieiqbal@gmail.com>
2020-01-28 21:03:10 +0100

    Complete connectors.py

--- Project - Optimizing Public Transportation/producers/connector.py
+++ Project - Optimizing Public Transportation/producers/connector.py
@@ -4,12 +4,11 @@ import logging
 
 import requests
 
-
 logger = logging.getLogger(__name__)
-
 
 KAFKA_CONNECT_URL = "http://localhost:8083/connectors"
 CONNECTOR_NAME = "stations"
+
 
 def configure_connector():
     """Starts and configures the Kafka Connect connector"""
@@ -21,53 +20,53 @@ def configure_connector():
         return
 
     # TODO: Complete the Kafka Connect Config below.
-    # Directions: Use the JDBC Source Connector to connect to Postgres. Load the `stations` table
-    # using incrementing mode, with `stop_id` as the incrementing column name.
-    # Make sure to think about what an appropriate topic prefix would be, and how frequently Kafka
-    # Connect should run this connector (hint: not very often!)
-        return
 
-    # TODO: Complete the Kafka Connect Config below.
-    # Directions: Use the JDBC Source Connector to connect to Postgres. Load the `stations` table
-    # using incrementing mode, with `stop_id` as the incrementing column name.
-    # Make sure to think about what an appropriate topic prefix would be, and how frequently Kafka
-    # Connect should run this connector (hint: not very often!)
-    logger.info("connector code not completed skipping connector creation")
-    #resp = requests.post(
-    #    KAFKA_CONNECT_URL,
-    #    headers={"Content-Type": "application/json"},
-    #    data=json.dumps({
-    #        "name": CONNECTOR_NAME,
-    #        "config": {
-    #            "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
-    #            "key.converter": "org.apache.kafka.connect.json.JsonConverter",
-    #            "key.converter.schemas.enable": "false",
-    #            "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-    #            "value.converter.schemas.enable": "false",
-    #            "batch.max.rows": "500",
-    #            # TODO
-    #            "connection.url": "",
-    #            # TODO
-    #            "connection.user": "",
-    #            # TODO
-    #            "connection.password": "",
-    #            # TODO
-    #            "table.whitelist": "",
-    #            # TODO
-    #            "mode": "",
-    #            # TODO
-    #            "incrementing.column.name": "",
-    #            # TODO
-    #            "topic.prefix": "",
-    #            # TODO
-    #            "poll.interval.ms": "",
-    #        }
-    #    }),
-    #)
+    # Directions: Use the JDBC Source Connector to connect to Postgres. Load the
+    # `stations` table using incrementing mode, with `stop_id` as the
+    # incrementing column name. Make sure to think about what an appropriate
+    # topic prefix would be, and how frequently Kafka Connect should run this
+    # connector (hint: not very often!)
 
-    ## Ensure a healthy response was given
-    #resp.raise_for_status()
-    #logging.debug("connector created successfully")
+    connection_class = "io.confluent.connect.jdbc.JdbcSourceConnector"
+    json_converter = "org.apache.kafka.connect.json.JsonConverter"
+
+    resp = requests.post(
+        KAFKA_CONNECT_URL,
+        headers={"Content-Type": "application/json"},
+        data=json.dumps(
+            {
+                "name": CONNECTOR_NAME,
+                "config": {
+                    "connector.class": connection_class,
+                    "key.converter": json_converter,
+                    "key.converter.schemas.enable": "false",
+                    "value.converter": json_converter,
+                    "value.converter.schemas.enable": "false",
+                    "batch.max.rows": "500",
+                    # TODO
+                    "connection.url": "jdbc:postgresql://postgres:5432/cta",
+                    # TODO
+                    "connection.user": "cta_admin",
+                    # TODO
+                    "connection.password": "chicago",
+                    # TODO
+                    "table.whitelist": "stations",
+                    # TODO
+                    "mode": "incrementing",
+                    # TODO
+                    "incrementing.column.name": "stop_id",
+                    # TODO
+                    "topic.prefix": "org.chicago.cta.",
+                    # TODO
+                    "poll.interval.ms": "60000",
+                },
+            },
+        ),
+    )
+
+    # Ensure a healthy response was given
+    resp.raise_for_status()
+    logging.debug("connector created successfully")
 
 
 if __name__ == "__main__":
